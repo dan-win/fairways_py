ff95bb8d91d067e2159c6f95b7babc5051d57f3f
danwin <dmitry.zimoglyadov@gmail.com>
2019-09-21 19:59:29 +0300

    package name fix

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 requirements = ["python>=3.6"]
 
-setup(name='fairways_py',
+setup(name='fairways',
       version='0.2',
       description='Generator for digital vouchers',
       url='https://gitlab.com/danwin/fairways_py',
       author='Dmitry Zimoglyadov',
       author_email='dmitry.zimoglyadov@gmail.com',
       license='MIT',
-      packages=['fairways_py'],
+      packages=['fairways'],
       zip_safe=False)
