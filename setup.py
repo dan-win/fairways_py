46dfc81980010506824cbe58d39adbb303751e25
Дмитрий Зимоглядов <zimoglyadov.d@tvzavr.ru>
2019-09-25 19:32:24 +0300

    v 0.8 label

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 requirements = ["python>=3.6"]
 
 setup(name='fairways',
-      version='0.2',
+      version='0.8',
       description='Toolset to organize batch tasks',
       url='https://gitlab.com/danwin/fairways_py#egg=fairways',
       author='Dmitry Zimoglyadov',
