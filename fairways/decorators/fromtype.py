2a9d8ed4e1e9aba8e7198a0871c64e34bf21df30
Dmitry Zimoglyadov <dmitry.zimoglyadov@gmail.com>
2019-11-11 20:22:17 +0300

    fromtype cleanup and draft note

--- fairways/decorators/fromtype.py
+++ fairways/decorators/fromtype.py
@@ -1,13 +1,4 @@
 
-from collections import defaultdict
-
-# from typing import Dict, Mapping, Callable
-
-
-# transform_rules_map: Dict[str, Callable]...
-
-
-
 
 class FromTypeMixin:
 
@@ -27,6 +18,18 @@ class FromTypeMixin:
         method(instance, source)
         return instance
 
+
+    # def intotype(self, dest_type):
+    #     key1 = cls.__qualname__
+    #     key2 = type(dest_type)
+    #     reg = FromTypeMixin.converters_registry
+    #     try:
+    #         method = reg[key1][key2]
+    #     except Exception:
+    #         raise TypeError(f"Cannot convert {self.__class__.__name__} into {dest_type}")
+    #     instance = dest_type()
+    #     method(instance, source)
+    #     return instance
 
 
 class implement:
@@ -49,39 +52,3 @@ class implement:
         root[key2] = f
         return f
 
-
-if __name__ == "__main__":
-    class CustomClass:
-        def __init__(self, name):
-            self.name = name
-        def __str__(self):
-            return f"{self.__class__.__name__}:{self.name}"
-
-    class MyClass(FromTypeMixin):
-
-        def __init__(self, value=None):
-            self.value = value
-
-        @implement_from(int)
-        def _from_int(self, value):
-            self.value = f"From int: {value}"
-
-        @implement_from(str)
-        def _from_str(self, value):
-            self.value = f"From str: {value}"
-
-        @implement_from(dict)
-        def _from_dict(self, value):
-            self.value = f"From dict: {value}"
-
-        @implement_from(CustomClass)
-        def _from_obj(self, value):
-            self.value = f"From CustomClass: {value}"
-        
-        def __str__(self):
-            return self.value
-
-    print(MyClass.fromtype(1))
-    print(MyClass.fromtype("Text"))
-    print(MyClass.fromtype(dict(a=1)))
-    print(MyClass.fromtype(CustomClass("objectName")))
