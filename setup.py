2a220177f0ea8f41bb87505018ba76b0a1a7e656
Dmitry Zimoglyadov <dmitry.zimoglyadov@gmail.com>
2019-11-11 20:37:26 +0300

    0.9.7

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ from setuptools import setup, find_packages
 requirements = ["python>=3.6"]
 
 setup(name='fairways',
-      version='0.9.6',
+      version='0.9.7',
       description='Toolset to organize tasks',
       url='https://gitlab.com/danwin/fairways_py#egg=fairways',
       author='Dmitry Zimoglyadov',
       author_email='dmitry.zimoglyadov@gmail.com',
       license='Apache 2.0',
       packages=find_packages(),
+      install_requires=[
+            'cached-property>=1.5.1',
+            'colorlog>=4.0.2',
+            'python-dateutil>=2.8.0',
+            'python-dotenv>=0.10.3',
+            'requests>=2.22.0'
+      ],
       zip_safe=False)
